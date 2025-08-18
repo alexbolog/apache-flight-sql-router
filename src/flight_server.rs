85b6ceabbd77049a3b31de1ec370daf83203017d
ablm <mbolog.alex@gmail.com>
2025-08-18 21:33:16 +0300

    added sqlBackend trait

--- src/flight_server.rs
+++ src/flight_server.rs
@@ -115,6 +115,25 @@ impl MyFlightService {
     }
 }
 
+use async_trait::async_trait;
+use arrow_array::RecordBatch;
+use arrow_schema::SchemaRef;
+
+pub type BatchStream =
+Pin<Box<dyn Stream<Item = anyhow::Result<RecordBatch>> + Send + 'static>>;
+
+#[async_trait]
+pub trait SqlBackend: Send + Sync {
+    fn name(&self) -> &'static str;
+
+    /// Discover the Arrow schema for a SQL statement.
+    async fn schema(&self, sql: &str, ctx: &AuthContext) -> anyhow::Result<SchemaRef>;
+
+    /// Execute and stream Arrow record batches.
+    async fn query(&self, sql: &str, ctx: &AuthContext) -> anyhow::Result<BatchStream>;
+}
+
+
 #[tonic::async_trait]
 impl FlightService for MyFlightService {
     type HandshakeStream =
